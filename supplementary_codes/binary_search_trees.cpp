a82fbbded001fff4082d8a105087b57abb53ba82
Kaushal Kishore <kshr4kshl@gmail.com>
2017-12-15 00:59:01 +0530

    BinarySearchTree() : class declaration

--- supplementary_codes/binary_search_trees.cpp
+++ supplementary_codes/binary_search_trees.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 template <typename Key>
 class BinarySearchTree {
-private:
+public:
     struct BinaryNode {
         Key element;     // The data in the node
         BinaryNode *left;   // left child
@@ -20,7 +20,6 @@ private:
         BinaryNode(Key && theElement , BinaryNode *lt, BinaryNode *rt, BinaryNode *p)
         : element{std::move (theElement)} , left{lt}, right{rt}, parent{p} {}
     } ;
-    BinaryNode *root;
 
     void inOrderTreeWalk(BinaryNode* x = nullptr , ostream & out = cout) const;
     void preOrderTreeWalk(BinaryNode* x = nullptr, ostream & out = cout) const;
@@ -28,29 +27,41 @@ private:
 
     void transplant(BinaryNode *u, BinaryNode *v);
 
-public:
+    BinaryNode *root;
+    BinarySearchTree(){
+        root = nullptr;
+    }
     ~BinarySearchTree(){
-        clear(root);
+        clear();
     }
     void printTree(ostream & out = cout  , int type = INORDER) const;
     BinaryNode* treeSearch(const Key &k , BinaryNode *x) const;
     int height(BinaryNode *t );
     BinaryNode* findMin(BinaryNode *x = nullptr) const;
     BinaryNode* findMax(BinaryNode *x = nullptr) const;
-    BinaryNode* succesor(BinaryNode *x = nullptr) const;
+    BinaryNode* successor(BinaryNode *x = nullptr) const;
     BinaryNode* predecessor(BinaryNode *x = nullptr) const;
     void remove(BinaryNode *z);
     void insert(const Key &k);
     bool contains(const Key &k ) const;
     bool isEmpty() const;
-    void clear(BinaryNode *x=nullptr);
+    void clearSubTree(BinaryNode *x=nullptr);
+    void clear();
+
 };
 
 template <typename Key>
-void BinarySearchTree<Key>::clear(BinaryNode *x){
-    if(x==nullptr)  x = root;
-    clear(x->left);
-    clear(x->right);
+void BinarySearchTree<Key>::clear(){
+    clearSubTree(root);
+    root = nullptr;
+}
+
+
+template <typename Key>
+void BinarySearchTree<Key>::clearSubTree(BinaryNode *x){
+    if(x==nullptr)  return ;
+    clearSubTree(x->left);
+    clearSubTree(x->right);
     delete x;
 }
 
@@ -133,7 +144,7 @@ typename BinarySearchTree<Key>::BinaryNode* BinarySearchTree<Key>::predecessor(B
 }
 
 template <typename Key>
-typename BinarySearchTree<Key>::BinaryNode* BinarySearchTree<Key>::succesor(BinaryNode *x) const {
+typename BinarySearchTree<Key>::BinaryNode* BinarySearchTree<Key>::successor(BinaryNode *x) const {
     if(x==nullptr) return nullptr;
     if(x->right != nullptr) return findMin(x->right);
     BinaryNode* y = x->parent ;
@@ -164,8 +175,8 @@ typename BinarySearchTree<Key>::BinaryNode* BinarySearchTree<Key>::findMax(Binar
 
 template <typename Key>
 typename BinarySearchTree<Key>::BinaryNode* BinarySearchTree<Key>::treeSearch(const Key &k , BinaryNode *x) const {
-    while(x!=nullptr and k!=x->key){
-        if(k < x->key) x = x->left;
+    while(x!=nullptr && k!=x->element){
+        if(k < x->element) x = x->left;
         else x = x->right;
     }
     return x;
@@ -174,11 +185,12 @@ typename BinarySearchTree<Key>::BinaryNode* BinarySearchTree<Key>::treeSearch(co
 template <typename Key>
 void BinarySearchTree<Key>::printTree(ostream & out , int type) const {
     switch(type) {
-        case INORDER : inOrderTreeWalk(root , out);
-        case PREORDER : preOrderTreeWalk(root , out);
-        case POSTORDER : postOrderTreeWalk(root ,out);
-        default: inOrderTreeWalk(out);
+        case INORDER : inOrderTreeWalk(root , out); break;
+//        case PREORDER : preOrderTreeWalk(root , out); break;
+//        case POSTORDER : postOrderTreeWalk(root ,out); break;
+        default: inOrderTreeWalk(root , out); break;
     }
+    cout<<endl;
 }
 
 // print a sorted list of the elements in the binary search tree
@@ -186,7 +198,7 @@ template <typename Key>
 void BinarySearchTree<Key>::inOrderTreeWalk(BinaryNode* x , ostream &out ) const {
     if(x!=nullptr) {
         inOrderTreeWalk(x->left);
-        out<<x.element<<" ";
+        out<<x->element<<" ";
         inOrderTreeWalk(x->right);
     }
 }
@@ -195,4 +207,77 @@ template <typename Key>
 int BinarySearchTree<Key>::height(BinaryNode *t){
     if(t==nullptr) return -1;
     else return 1 + max(height(t->left) , height(t->right));
+}
+
+
+int main() {
+    BinarySearchTree<int> bst;
+
+    cout<<"Is Tree Empty: "<<bst.isEmpty()<<endl;
+    cout<<"Inserting Some elements..."<<endl;
+
+    int arr[] = {54 , 78 , 90 , 67 , 12, 100 , 43 , 10 , 1, 27};
+    for(int i=0; i<10; i++){
+        cout<<"Inserting : "<<i<<endl;
+        bst.insert(arr[i]);
+    }
+
+    cout<<"Is Tree Empty: "<<bst.isEmpty()<<endl;
+
+    cout<<"\nPrinting the binary tree inorder-walk: "<<endl;
+    bst.printTree();
+    cout <<endl;
+
+
+    // finding the minimum
+    cout<<"Minimum element of the Tree: "<< (bst.findMin(bst.root))->element << endl;
+
+    // finding the maximum
+    cout<<"Maximum element of the Tree: "<< (bst.findMax(bst.root))->element << endl;
+
+    // height of the tree
+    cout<<"Height of the Tree: "<<bst.height(bst.root)<<endl;
+
+    cout<<"Does tree contains 12: "<<bst.contains(12)<<endl;
+    cout<<"Does tree contains 122: "<<bst.contains(122)<<endl;
+
+    //cout<<endl;
+    // removing some of the elements
+    BinarySearchTree<int>::BinaryNode* temp = bst.treeSearch(43 , bst.root);
+    cout<<"What is successor of 43? "<<(bst.successor(temp))->element<<endl;
+    cout<<"What is predecessor of 43? "<<(bst.predecessor(temp))->element<<endl;
+    temp = bst.treeSearch(1 , bst.root);
+    cout<<"What is predecessor of 1? ";
+    if(bst.predecessor(temp)!=nullptr) cout<<(bst.predecessor(temp))->element<<endl;
+    else cout<<"No successor for 100\n";
+    temp = bst.treeSearch(100 , bst.root);
+    cout<<"What is successor of 100? ";
+    if(bst.successor(temp)!=nullptr) cout<<(bst.successor(temp))->element<<endl;
+    else cout<<"No successor for 100\n";
+    temp = bst.treeSearch(43 , bst.root);
+    cout<<"\nRemoving some of the elements { 43 }. \nPrinting the modified tree: "<<endl;
+
+    bst.remove( temp );
+    bst.printTree();
+
+    cout<<"After removing 90 : ";
+    bst.remove( bst.treeSearch(90 , bst.root) );
+    bst.printTree();
+
+    // clearing the tree
+    cout<<"Deleting all the elements in the tree.."<<endl;
+    bst.clear();
+
+    cout<<"\nIs Tree Empty: "<<bst.isEmpty()<<endl;
+    cout<<"Inserting Some elements..."<<endl;
+
+    // adding more elements
+    for (int i=1; i<1000 ; i*=3){
+        bst.insert(i);
+        bst.insert(i+27);
+    }
+
+    // printing after the inserts
+    bst.printTree();
+    return 0;
 }
