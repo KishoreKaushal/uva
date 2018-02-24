fee8e9a0557a54c79f4ee2d509788faebc3a8197
Kaushal Kishore <kshr4kshl@gmail.com>
2018-02-24 21:15:45 +0530

    AC

---
+++ Problem-Solving-Paradigms/10976.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+typedef unsigned long long int luint;
+typedef pair<int,int> ii;
+
+bool comp(ii &a, ii &b) {
+    return a.second < b.second ;
+}
+// 1/2 = 1/6 + 1/3
+void print(const vector<ii> &v,const int &k){
+    for(auto itr = v.rbegin() ; itr!=v.rend(); ++itr) {
+        cout<<"1/"<<k<<" = "<<"1/"<<(*itr).first<<" + "<<"1/"<<(*itr).second<<endl;
+    }
+}
+
+int main() {
+    luint k ;
+    while(cin>>k) {
+        vector<ii> v;
+        for(luint y=2*k; y >= k+1 ; --y) {
+            if((k*y)%(y-k) == 0) {
+                v.push_back(ii((k*y)/(y-k) , y));
+            }
+        }
+        cout<<v.size()<<endl;
+        print(v, k);
+    }
+    return 0;
+}
