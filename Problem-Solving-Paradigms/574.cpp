1071e2758de88638246e697ab50f3f32cadc5ac7
KishoreKaushal <kshr4kshl@gmail.com>
2018-12-17 15:00:32 +0530

    Recursive Backtracking easy done

--- Problem-Solving-Paradigms/574.cpp
+++ Problem-Solving-Paradigms/574.cpp
@@ -4,22 +4,43 @@ using namespace std;
 
 typedef long long int lli;
 
-lli t, n;
+lli t, n, curr_sum=0;
 vector<lli> vec;
-map<lli,lli> ele_count;
-map<lli, lli, greater <lli>>::iterator it;
+map<lli,lli, greater<lli>> ele_count;
+map<lli, lli>::iterator it;
 vector<pair<lli, lli>> results;
+bool found = false;
 
 void backtrack(int N){
     if (N == 0){
-        /*print results*/
-        vector<pair<lli,lli>>::iterator it = results.begin();
-        cout<<it->first;
-        while (it != results.end()){
-            cout<<"+"<<it->
-        }
+        if (results.empty() || curr_sum != t) return;
+        found = true;
+        lli temp;
+        vector<pair<lli, lli>>::iterator it = results.begin();        
+        cout<<it->first;    temp = it->second-1;
+        while(temp--){
+            cout<<"+"<<it->first;
+        } ++it;
+        while(it != results.end()){
+            temp = it->second;
+            while(temp--){
+                cout<<"+"<<it->first;
+            }
+            ++it;
+        } cout<<endl;
     } else {
-        for 
+        lli curr_ele_count = it->second;
+        for (lli cnt=curr_ele_count; cnt >= 0; --cnt){
+            if (cnt!=0) results.push_back(make_pair(it->first, cnt));
+            curr_sum += cnt*it->first;
+            if (curr_sum <= t)  {
+                ++it;
+                backtrack(N-1);
+                --it;
+            }
+            curr_sum -= cnt*it->first;
+            if (cnt!=0) results.pop_back();
+        }
     }
 }
 
@@ -39,12 +60,13 @@ int main(){
             }
         }
 
-        sort(vec.begin(), vec.begin()+n, greater<int>());
-        
         lli distinct = ele_count.size();
-
+        it = ele_count.begin();
+        cout<<"Sums of "<<t<<":"<<endl;
+        curr_sum = 0;
+        found = false;
         backtrack(distinct);
-
+        if (!found) cout<<"NONE"<<endl;
     }
 
     return 0;
